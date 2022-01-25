7d76d92f6d81c2b690070e5a1d3fa0be483158f6
Handri-Bikemap <handri@bikemap.net>
2022-01-25 16:24:37 +0100

    Support for Android

--- core/src/main/java/com/graphhopper/GraphHopper.java
+++ core/src/main/java/com/graphhopper/GraphHopper.java
@@ -72,6 +72,7 @@ import static com.graphhopper.util.Parameters.Algorithms.RoundTrip;
  * @author Peter Karich
  */
 public class GraphHopper {
+    private static boolean isAndroid = false;
     private static final Logger logger = LoggerFactory.getLogger(GraphHopper.class);
     private MaxSpeedCalculator maxSpeedCalculator;
     private final Map<String, Profile> profilesByName = new LinkedHashMap<>();
@@ -132,6 +133,14 @@ public class GraphHopper {
     private String dateRangeParserString = "";
     private String encodedValuesString = "";
     private String vehiclesString = "";
+
+    public static boolean isAndroid() {
+        return isAndroid;
+    }
+
+    public static void setIsAndroid(boolean isAndroid) {
+        GraphHopper.isAndroid = isAndroid;
+    }
 
     public GraphHopper setEncodedValuesString(String encodedValuesString) {
         this.encodedValuesString = encodedValuesString;
