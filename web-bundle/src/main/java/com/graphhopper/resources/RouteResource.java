50a6ee4007bf04352d808424e4243c86a67fac43
Benedikt Brandtner <benedikt@bikemap.net>
2022-08-08 16:48:35 +0200

    added mapbox navigation sdk response to RouteResource POST endpoint
    Signed-off-by: Benedikt Brandtner <benedikt@bikemap.net>

--- web-bundle/src/main/java/com/graphhopper/resources/RouteResource.java
+++ web-bundle/src/main/java/com/graphhopper/resources/RouteResource.java
@@ -24,6 +24,9 @@ import com.graphhopper.gpx.GpxConversions;
 import com.graphhopper.http.GHPointParam;
 import com.graphhopper.jackson.MultiException;
 import com.graphhopper.jackson.ResponsePathSerializer;
+import com.graphhopper.navigation.DistanceConfig;
+import com.graphhopper.navigation.DistanceUtils;
+import com.graphhopper.navigation.NavigateResponseConverter;
 import com.graphhopper.routing.ProfileResolver;
 import com.graphhopper.util.*;
 import com.graphhopper.util.shapes.GHPoint;
@@ -38,6 +41,7 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import static com.graphhopper.util.Parameters.Details.PATH_DETAILS;
@@ -59,12 +63,14 @@ public class RouteResource {
     private final GraphHopperAPI graphHopper;
     private final ProfileResolver profileResolver;
     private final Boolean hasElevation;
+    private final TranslationMap translationMap;
 
     @Inject
-    public RouteResource(GraphHopperAPI graphHopper, ProfileResolver profileResolver, @Named("hasElevation") Boolean hasElevation) {
+    public RouteResource(GraphHopperAPI graphHopper, ProfileResolver profileResolver, @Named("hasElevation") Boolean hasElevation, TranslationMap translationMap) {
         this.graphHopper = graphHopper;
         this.profileResolver = profileResolver;
         this.hasElevation = hasElevation;
+        this.translationMap = translationMap;
     }
 
     @GET
@@ -179,6 +185,7 @@ public class RouteResource {
         boolean enableElevation = request.getHints().getBool("elevation", false);
         boolean calcPoints = request.getHints().getBool(CALC_POINTS, true);
         boolean pointsEncoded = request.getHints().getBool("points_encoded", true);
+        String responseType = request.getHints().getString("type", "json");
 
         long took = sw.stop().getNanos() / 1_000_000;
         String infoStr = httpReq.getRemoteAddr() + " " + httpReq.getLocale() + " " + httpReq.getHeader("User-Agent");
@@ -193,7 +200,15 @@ public class RouteResource {
 
         if (ghResponse.hasErrors()) {
             logger.error(logStr + ", errors:" + ghResponse.getErrors());
-            throw new MultiException(ghResponse.getErrors());
+
+            if (responseType.equals("mapbox")) {
+                // Mapbox specifies 422 return type for input errors
+                return Response.status(422).entity(NavigateResponseConverter.convertFromGHResponseError(ghResponse)).
+                        header("X-GH-Took", "" + Math.round(took * 1000)).
+                        build();
+            } else {
+                throw new MultiException(ghResponse.getErrors());
+            }
         } else {
             logger.info(logStr + ", alternatives: " + ghResponse.getAll().size()
                     + ", distance0: " + ghResponse.getBest().getDistance()
@@ -201,10 +216,28 @@ public class RouteResource {
                     + ", time0: " + Math.round(ghResponse.getBest().getTime() / 60000f) + "min"
                     + ", points0: " + ghResponse.getBest().getPoints().getSize()
                     + ", debugInfo: " + ghResponse.getDebugInfo());
-            return Response.ok(ResponsePathSerializer.jsonObject(ghResponse, instructions, calcPoints, enableElevation, pointsEncoded, took)).
-                    header("X-GH-Took", "" + Math.round(took)).
-                    type(MediaType.APPLICATION_JSON).
-                    build();
+
+            if (responseType.equals("mapbox")) {
+                String voiceUnits = request.getHints().getString("voice_units", "metric");
+                DistanceUtils.Unit unit;
+                if (voiceUnits.equals("metric")) {
+                    unit = DistanceUtils.Unit.METRIC;
+                } else {
+                    unit = DistanceUtils.Unit.IMPERIAL;
+                }
+
+                Locale locale = request.getLocale();
+
+                DistanceConfig config = new DistanceConfig(unit, translationMap, locale);
+                return Response.ok(NavigateResponseConverter.convertFromGHResponse(ghResponse, translationMap, locale, config)).
+                        header("X-GH-Took", "" + Math.round(took * 1000)).
+                        build();
+            } else {
+                return Response.ok(ResponsePathSerializer.jsonObject(ghResponse, instructions, calcPoints, enableElevation, pointsEncoded, took)).
+                        header("X-GH-Took", "" + Math.round(took)).
+                        type(MediaType.APPLICATION_JSON).
+                        build();
+            }
         }
     }
 
