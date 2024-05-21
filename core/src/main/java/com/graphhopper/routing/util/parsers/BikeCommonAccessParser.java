540bc91e072e58fcc7d96b68c8d7243766ee8b74
Benedikt Brandtner <benedikt@bikemap.net>
2024-05-21 19:29:47 +0200

    removed "agricultural" and "forestry" from restricted values in BikeCommonAccessParser

--- core/src/main/java/com/graphhopper/routing/util/parsers/BikeCommonAccessParser.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/BikeCommonAccessParser.java
@@ -22,8 +22,6 @@ public abstract class BikeCommonAccessParser extends AbstractAccessParser implem
 
         this.roundaboutEnc = roundaboutEnc;
 
-        restrictedValues.add("agricultural");
-        restrictedValues.add("forestry");
         restrictedValues.add("delivery");
 
         barriers.add("fence");
