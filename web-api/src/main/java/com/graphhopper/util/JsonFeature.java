6601d65fe364cb797451520e950f387a7d17dc95
Oleksii Kirizii <oleksii@bikemap.net>
2025-10-03 13:00:21 +0200

    Remove another usage of javax.lang.model.SourceVersion.

--- web-api/src/main/java/com/graphhopper/util/JsonFeature.java
+++ web-api/src/main/java/com/graphhopper/util/JsonFeature.java
@@ -20,7 +20,8 @@ package com.graphhopper.util;
 import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geom.Geometry;
 
-import javax.lang.model.SourceVersion;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
 
 import static java.lang.Character.isDigit;
@@ -95,7 +96,7 @@ public class JsonFeature {
     }
 
     public static boolean isValidId(String name) {
-        if (name.length() <= 3 || !name.startsWith("in_") || SourceVersion.isKeyword(name)) return false;
+        if (name.length() <= 3 || !name.startsWith("in_") || UniversalSourceVersion.isKeyword(name)) return false;
 
         int underscoreCount = 0;
         for (int i = 1; i < name.length(); i++) {
@@ -110,5 +111,20 @@ public class JsonFeature {
             }
         }
         return true;
+    }
+
+    private static class UniversalSourceVersion {
+        private static final HashSet<String> JAVA_KEYWORDS = new HashSet<>(Arrays.asList(
+                "strictfp", "assert", "enum", "_", "public", "protected", "private", "abstract",
+                "static", "final", "transient", "volatile", "synchronized", "native", "class",
+                "interface", "extends", "package", "throws", "implements", "boolean", "byte", "char",
+                "short", "int", "long", "float", "double", "void", "if", "else", "try", "catch",
+                "finally", "do", "while", "for", "continue", "switch", "case", "default", "break",
+                "throw", "return", "this", "new", "super", "import", "instanceof", "goto", "const",
+                "null", "true", "false"));
+
+        public static boolean isKeyword(String name) {
+            return JAVA_KEYWORDS.contains(name);
+        }
     }
 }
